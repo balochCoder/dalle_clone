9091d64f3a242081d3ed4accd1faed7ae0e320cc
Admin <Admin@TAIMOOR>
2023-01-26 17:15:21 +0500

    first commit

---
+++ server/index.js
@@ -0,0 +1,38 @@
+import express from 'express'
+import * as dotenv from 'dotenv'
+
+import cors from 'cors'
+import connectDB from "./mongodb/connect.js";
+
+import postRoutes from './routes/postRoutes.js'
+import dalleRoutes from './routes/dalleRoutes.js'
+
+dotenv.config();
+
+const app = express();
+
+app.use(cors());
+app.use(express.json({
+    limit: '50mb'
+}))
+
+
+app.use('/api/v1/posts', postRoutes)
+app.use('/api/v1/dalle', dalleRoutes)
+app.get('/', (req, res) => {
+    res.send('Hello from DaLLE')
+})
+
+
+const startServer = () => {
+
+    try {
+        connectDB(process.env.MONGODB_URL);
+        app.listen(3000, () => console.log(`Server is running on Port 3000`))
+
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+startServer();
